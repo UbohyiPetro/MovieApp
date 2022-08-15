6517dfba21e45d9a9e774f5a1a198685ff3faf77
petro.ubohyi <petro.ubohyi@globallogic.com>
2022-08-15 17:40:36 +0300

    create movies list ui

---
+++ app/src/main/java/com/example/movieapp/ui/movies_list/MoviesListFragment.kt
@@ -0,0 +1,18 @@
+package com.example.movieapp.ui.movies_list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
